145a8ac067854bab550620e0bc4267d8e8d3d8b9
BoreyLee <475773037@qq.com>
2021-06-14 15:44:27 +0800

    Create que.js
    添加提问文件

---
+++ que.js
@@ -0,0 +1,22 @@
+module.exports = [
+  {
+    type: 'comfirm',
+    name: 'private',
+    message: '是否为私有仓库？'
+  },
+  {
+    type: 'input',
+    name: 'author',
+    message: 'author?'
+  },
+  {
+    type: 'input',
+    name: 'description',
+    message: 'description'
+  },
+  {
+    type: 'input',
+    name: 'license',
+    message: 'license?'
+  }
+]
